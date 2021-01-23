b955d82ad8fcf665cce15b4a03faadd361aeef45
Paul Rubio <paulrh1989@gmail.com>
2021-01-23 18:59:59 +0100

    Added test start match ok

---
+++ src/test/java/com/ryc/score/service/MatchServiceTests.java
@@ -0,0 +1,13 @@
+package com.ryc.score.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MatchServiceTests {
+
+    @Test
+    public void testStartMatchOK(){
+
+    }
+}
